34901ff8ee015fd95b8d79c4f4750e29b805e2c7
付建邦 <fujianbang0ops@gmail.com>
2022-02-02 11:42:37 +0800

    finished module Structures

---
+++ src/customtypes/structures/src/main.rs
@@ -0,0 +1,60 @@
+#[derive(Debug)]
+#[allow(dead_code)]
+struct Person {
+    name: String,
+    age: u8,
+}
+
+// unit struct
+struct Unit;
+
+// tuple struct
+struct Pair(i32, f32);
+
+// 普通结构体
+struct Point {
+    x: f32,
+    y: f32,
+}
+
+#[allow(dead_code)]
+struct Rectangle {
+    top_left: Point,
+    bottom_right: Point,
+}
+
+fn main() {
+    let name = String::from("Peter");
+    let age = 27;
+    let peter = Person { name, age };
+
+    println!("{:?}", peter);
+
+    let point: Point = Point { x: 10.3, y: 0.4 };
+    println!("point coordinates: ({}, {})", point.x, point.y);
+
+    // 创建新结构体，未主动声明的成员变量使用point的相关值为默认初始值
+    let bottom_right = Point { x: 5.2, ..point };
+    println!("second point: ({}, {})", bottom_right.x, bottom_right.y);
+
+    // 使用 let 解构
+    let Point { x: left_edge, y: top_edge } = point;
+
+    let _rectangle = Rectangle {
+        top_left: Point { x: left_edge, y: top_edge },
+        bottom_right,
+    };
+
+    let _unit = Unit;
+
+    let pair = Pair(1, 0.1);
+
+    // 访问 tuple struct （方式和tuple相同）
+    println!("pair contains {:?} and {:?}", pair.0, pair.1);
+
+    // 解构 tuple struct
+    // 可简写：let (integer, decimal) = pair;
+    let Pair(integer, decimal) = pair;
+
+    println!("pair contains {:?} and {:?}", integer, decimal);
+}
