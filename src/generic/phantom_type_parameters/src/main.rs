4d9a192cfc14c99eb223d794d5c2f436e4cd0915
付建邦 <fujianbang0ops@gmail.com>
2022-02-08 22:38:10 +0800

    finished module Generics - phantom type parameters

---
+++ src/generic/phantom_type_parameters/src/main.rs
@@ -0,0 +1,26 @@
+use std::marker::PhantomData;
+
+// PartialEq 允许该类型进行相等测试
+#[derive(PartialEq)]
+struct PhantomTuple<A, B>(A, PhantomData<B>);
+
+#[derive(PartialEq)]
+struct PhantomStruct<A, B> {
+    first: A,
+    phantom: PhantomData<B>,
+}
+
+fn main() {
+    let _tuple: PhantomTuple<char, f32> = PhantomTuple('Q', PhantomData);
+    let _tuple2: PhantomTuple<char, f64> = PhantomTuple('Q', PhantomData);
+
+    let _struct1: PhantomStruct<char, f32> = PhantomStruct {
+        first: 'Q',
+        phantom: PhantomData,
+    };
+
+    let _struct2: PhantomStruct<char, f64> = PhantomStruct {
+        first: 'Q',
+        phantom: PhantomData,
+    }
+}
