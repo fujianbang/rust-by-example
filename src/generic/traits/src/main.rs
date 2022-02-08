a3d1118ada200f14721727d8da2a5f18b347a3d1
付建邦 <fujianbang0ops@gmail.com>
2022-02-08 21:10:01 +0800

    finished module Generics - trait

--- src/generic/traits/src/main.rs
+++ src/generic/traits/src/main.rs
@@ -1,3 +1,21 @@
+struct Empty;
+
+struct Null;
+
+// `T` 的泛型 trait
+trait DoubleDrop<T> {
+    fn double_drop(self, _: T);
+}
+
+impl<T, U> DoubleDrop<T> for U {
+    // 此方法获得两个传入参数的所有权，并释放
+    fn double_drop(self, _: T) {}
+}
+
 fn main() {
-    println!("Hello, world!");
+    let empty = Empty;
+    let null = Null;
+
+    // 释放empty和null
+    empty.double_drop(null);
 }
