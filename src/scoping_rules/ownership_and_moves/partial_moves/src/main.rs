9f21e12e999fde4016b5b5b90b9d2d85e459f5c5
付建邦 <fujianbang0ops@gmail.com>
2022-02-08 23:13:36 +0800

    finished module Scope rules - ownership_and_moves - partial move

--- src/scoping_rules/ownership_and_moves/partial_moves/src/main.rs
+++ src/scoping_rules/ownership_and_moves/partial_moves/src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
-    println!("Hello, world!");
+    #[derive(Debug)]
+    struct Person {
+        name: String,
+        age: u8,
+    }
+
+    let person = Person {
+        name: String::from("Alice"),
+        age: 20,
+    };
+
+    // `name` 从 person 中移走，`age` 只是引用（通过ref）
+    let Person { name, ref age } = person;
+
+    println!("The person's age is {}", age);
+
+    println!("The person's name is {}", name);
+
+    // Error person部分值已经移动
+    // println!("The person struct is {:?}", person);
+
+    println!("The person's age from person struct is {}", person.age);
 }
