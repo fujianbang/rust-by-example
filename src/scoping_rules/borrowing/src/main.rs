f338eeba95f91c53f5d94dcd0e66e4d75795b817
付建邦 <fujianbang0ops@gmail.com>
2022-02-08 23:22:37 +0800

    finished module Scope rules - borrowing

--- src/scoping_rules/borrowing/src/main.rs
+++ src/scoping_rules/borrowing/src/main.rs
@@ -1,3 +1,31 @@
+// 取得box所有权并销毁
+fn eat_box_i32(boxed_i32: Box<i32>) {
+    println!("Destroying box that contains {}", boxed_i32);
+}
+
+// 借用一个i32
+fn borrow_i32(borrowed_i32: &i32) {
+    println!("This int is: {}", borrowed_i32);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let boxed_i32 = Box::new(5_i32);
+    let stacked_i32 = 6_i32;
+
+    // 借用了box，但没有取得所有权
+    borrow_i32(&boxed_i32);
+    borrow_i32(&stacked_i32);
+
+    {
+        // 获取 box 中数据的引用
+        let _ref_to_i32: &i32 = &boxed_i32;
+
+        // ERROR 当前作用域外借用，不能将其销毁
+        // eat_box_i32(boxed_i32);
+
+        borrow_i32(_ref_to_i32);
+        // _ref_to_i32 离开作用域不再被借用
+    }
+
+    eat_box_i32(boxed_i32);
 }
