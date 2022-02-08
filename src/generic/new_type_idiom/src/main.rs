0a4a48540133993fff985cddab2e05bb982b9131
付建邦 <fujianbang0ops@gmail.com>
2022-02-08 21:55:13 +0800

    finished module Generics - new type idiom

--- src/generic/new_type_idiom/src/main.rs
+++ src/generic/new_type_idiom/src/main.rs
@@ -1,3 +1,27 @@
+struct Years(i64);
+
+struct Days(i64);
+
+// newtype 为数据定义新的类型
+impl Years {
+    pub fn to_days(&self) -> Days {
+        Days(self.0 * 365)
+    }
+}
+
+impl Days {
+    pub fn to_years(&self) -> Years {
+        Years(self.0 / 365)
+    }
+}
+
+fn old_enough(age: &Years) -> bool {
+    age.0 >= 18
+}
+
 fn main() {
-    println!("Hello, world!");
+    let age = Years(5);
+    let age_days = age.to_days();
+    println!("Old enough {}", old_enough(&age));
+    println!("Old enough {}", old_enough(&age_days.to_years()));
 }
