9d342de2d7fd7419b2a4c8835600eca58e18bbb4
付建邦 <fujianbang0ops@gmail.com>
2022-02-08 23:07:11 +0800

    finished module Scope rules - ownership_and_moves

--- src/scoping_rules/ownership_and_moves/src/main.rs
+++ src/scoping_rules/ownership_and_moves/src/main.rs
@@ -1,3 +1,28 @@
+// 函数取得堆分配的内存所有权
+fn destroy_box(c: Box<i32>) {
+    println!("Destroy a box that contains {}", c);
+
+    // `c` 被销毁，内存释放
+}
+
 fn main() {
-    println!("Hello, world!");
+    // 栈分配的整型u32
+    let x = 5u32;
+
+    // `x` 复制到 `y`，没有资源移动
+    let y = x;
+
+    // 两个值自由使用
+    println!("x is {}, and y is {}", x, y);
+
+
+    // `a` 指向堆分配堆整型的指针
+    let a = Box::new(5i32);
+
+    println!("a contains: {}", a);
+
+    // 所有权移动，`a` 释放
+    let b = a;
+
+    destroy_box(b);
 }
