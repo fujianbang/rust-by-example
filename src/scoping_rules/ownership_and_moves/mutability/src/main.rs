12f3e94509b191183574005904343f6bd4e5cdb9
付建邦 <fujianbang0ops@gmail.com>
2022-02-08 23:09:57 +0800

    finished module Scope rules - ownership_and_moves - mutability

--- src/scoping_rules/ownership_and_moves/mutability/src/main.rs
+++ src/scoping_rules/ownership_and_moves/mutability/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let immutable_box = Box::new(5u32);
+
+    println!("immutable_box contains {}", immutable_box);
+
+    // 所有权改变，数据可变性也发生了改变
+    let mut mutable_box = immutable_box;
+
+    *mutable_box = 4;
+
+    println!("mutable_box now contains {}", mutable_box);
 }
