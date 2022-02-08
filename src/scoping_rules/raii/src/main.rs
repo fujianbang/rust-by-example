e1ac926920a9bfa9221d41ab5bcc94aa2cff9822
付建邦 <fujianbang0ops@gmail.com>
2022-02-08 22:58:31 +0800

    finished module Scope rules - raii

---
+++ src/scoping_rules/raii/src/main.rs
@@ -0,0 +1,23 @@
+fn create_box() {
+    let _box1 = Box::new(3i32);
+
+    // _box1 销毁，释放内存
+}
+
+fn main() {
+    // 堆上分配一个整型数据
+    let _box2 = Box::new(5i32);
+
+    // 嵌套作用域
+    {
+        let _box3 = Box::new(4i32);
+
+        // _box3 内存释放
+    }
+
+    for _ in 0u32..1_00 {
+        create_box()
+    }
+
+    // _box2 销毁，释放内存
+}
