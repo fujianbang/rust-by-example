6eb73eb1bf4213ee100a9b1b154aff31458ee7b7
付建邦 <fujianbang0ops@gmail.com>
2022-02-08 21:28:55 +0800

    finished module Generics - multi_bounds

--- src/generic/multiple_bounds/src/main.rs
+++ src/generic/multiple_bounds/src/main.rs
@@ -1,3 +1,22 @@
+use std::fmt::{Debug, Display};
+
+// `+` 添加多重约束
+fn compare_prints<T: Debug + Display>(t: &T) {
+    println!("Debug: `{:?}`", t);
+    println!("Display: `{}`", t);
+}
+
+fn compare_types<T: Debug, U: Debug>(t: &T, u: &U) {
+    println!("t: `{:?}`", t);
+    println!("u: `{:?}`", u);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let string = "words";
+    let array = [1, 2, 3];
+    let vec = vec![1, 2, 3];
+
+    compare_prints(&string);
+
+    compare_types(&array, &vec);
 }
