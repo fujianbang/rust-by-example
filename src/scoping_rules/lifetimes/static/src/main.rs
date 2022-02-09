30ae1ba676a7c673652c2d8af7cc7d5bb7ed9194
付建邦 <fujianbang0ops@gmail.com>
2022-02-09 23:48:14 +0800

    finished module Scope rules - lifetime - static

--- src/scoping_rules/lifetimes/static/src/main.rs
+++ src/scoping_rules/lifetimes/static/src/main.rs
@@ -1,3 +1,24 @@
+static NUM: i32 = 18;
+
+fn coerce_static<'a>(_: &'a i32) -> &'a i32 {
+    &NUM
+}
+
 fn main() {
-    println!("Hello, world!");
+    {
+        let static_string = "I'm in read-only memory";
+        println!("static_string: {}", static_string);
+
+        // static_string 离开作用域，引用不能再使用，但是数据仍然存在二进制文件
+    }
+    {
+        let lifetime_num = 9;
+
+        // 对 NUM 的引用被强制转换成 lifetime_num 的生命周期
+        let coerced_static = coerce_static(&lifetime_num);
+
+        println!("coerced_static: {}", coerced_static);
+    }
+
+    println!("NUM: {} stays accessible!", NUM);
 }
