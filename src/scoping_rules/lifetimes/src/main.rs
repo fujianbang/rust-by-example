db9da84c0991b29c39c958409997d2f465202b7d
付建邦 <fujianbang0ops@gmail.com>
2022-02-09 22:41:28 +0800

    finished module Scope rules - lifetime

--- src/scoping_rules/lifetimes/src/main.rs
+++ src/scoping_rules/lifetimes/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    let i = 3;
+
+    {
+        let borrow1 = &i; // borrow1 生命周期开始
+        println!("borrow1: {}", borrow1);
+
+        // borrow1 生命周期结束
+    }
+
+    {
+        let borrow2 = &i;
+
+        println!("borrow2: {}", borrow2);
+    }
+
+
+    // i 变量生命周期结束
 }
