9900b2830d71be05acf67c76d1286d8bee06f34b
付建邦 <fujianbang0ops@gmail.com>
2022-02-09 23:24:26 +0800

    finished module Scope rules - lifetime - functions

--- src/scoping_rules/lifetimes/functions/src/main.rs
+++ src/scoping_rules/lifetimes/functions/src/main.rs
@@ -1,3 +1,33 @@
+// 'a 存活时间至少和函数一样长
+fn print_one<'a>(x: &'a i32) {
+    println!("`print_one`: x is {}", x);
+}
+
+// 可变引用也可能拥有声明周期
+fn add_one<'a>(x: &'a mut i32) {
+    *x += 1
+}
+
+fn print_multi<'a, 'b>(x: &'a i32, y: &'b i32) {
+    println!("`print_multi`: x is {}, y is {}", x, y);
+}
+
+// 必须返回正确的生命周期
+fn pass_x<'a, 'b>(x: &'a i32, _: &'b i32) -> &'a i32 {
+    x
+}
+
 fn main() {
-    println!("Hello, world!");
+    let x = 7;
+    let y = 9;
+
+    print_one(&x);
+    print_multi(&x, &y);
+
+    let z = pass_x(&x, &y);
+    print_one(z);
+
+    let mut t = 3;
+    add_one(&mut t);
+    print_one(&t);
 }
