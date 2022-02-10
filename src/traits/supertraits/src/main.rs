fc36d2c1fa2f6831d1afb9197377fdfd670ec1d4
付建邦 <fujianbang0ops@gmail.com>
2022-02-10 23:53:00 +0800

    finished module Traits - super trait

--- src/traits/supertraits/src/main.rs
+++ src/traits/supertraits/src/main.rs
@@ -1,3 +1,29 @@
+trait Person {
+    fn name(&self) -> String;
+}
+
+trait Student: Person {
+    fn university(&self) -> String;
+}
+
+trait Programmer {
+    fn fav_language(&self) -> String;
+}
+
+trait CompSciStudent: Programmer + Student {
+    fn git_username(&self) -> String;
+}
+
+fn com_sci_student_greeting(student: &dyn CompSciStudent) -> String {
+    format!(
+        "My name is {} and I attend {}. My favorite language is {}. My git username is {}",
+        student.name(),
+        student.university(),
+        student.fav_language(),
+        student.git_username()
+    )
+}
+
 fn main() {
     println!("Hello, world!");
 }
