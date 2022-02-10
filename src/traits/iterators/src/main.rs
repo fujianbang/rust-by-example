6883026bbd037a8a7ce76d83c34012ccce7a721b
付建邦 <fujianbang0ops@gmail.com>
2022-02-10 23:36:19 +0800

    finished module Traits - iterators

--- src/traits/iterators/src/main.rs
+++ src/traits/iterators/src/main.rs
@@ -1,3 +1,57 @@
+struct Fibonacci {
+    curr: u32,
+    next: u32,
+}
+
+impl Iterator for Fibonacci {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let new_next = self.curr + self.next;
+
+        self.curr = self.next;
+        self.next = new_next;
+
+        Some(self.curr)
+    }
+}
+
+fn fibonacci() -> Fibonacci {
+    Fibonacci { curr: 1, next: 1 }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut sequence = 0..3;
+
+    println!("Four consecutive `next` calls on 0..3");
+    println!("> {:?}", sequence.next());
+    println!("> {:?}", sequence.next());
+    println!("> {:?}", sequence.next());
+    println!("> {:?}", sequence.next());
+
+    // `for` 遍历
+    println!("Iterate through 0..3 using `for`");
+    for i in 0..3 {
+        println!("> {}", i);
+    }
+
+    // `take(n)` 方法提取 `Iterator`的前`n`项
+    println!("The first four terms of the Fibonacci sequence are: ");
+    for i in fibonacci().take(4) {
+        println!("> {}", i);
+    }
+
+    // `skip(n)` 移除前n项
+    println!("The next four terms of the Fibonacci sequence are: ");
+    for i in fibonacci().skip(4).take(4) {
+        println!("> {}", i);
+    }
+
+    let array = [1u32, 3, 4, 7];
+
+    println!("Iterate the following array {:?}", &array);
+    // `iter()` 方法对数组/slice产生一个`iterator`
+    for i in array.iter() {
+        println!("> {}", i);
+    }
 }
