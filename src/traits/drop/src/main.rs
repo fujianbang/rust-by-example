6418e7c199ce25ee661bb42e079a7c4fca63dd6b
付建邦 <fujianbang0ops@gmail.com>
2022-02-10 23:27:44 +0800

    finished module Traits - drop

--- src/traits/drop/src/main.rs
+++ src/traits/drop/src/main.rs
@@ -1,3 +1,37 @@
+struct Droppable {
+    name: &'static str,
+}
+
+impl Drop for Droppable {
+    fn drop(&mut self) {
+        println!("> Dropping {}", self.name);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let _a = Droppable { name: "a" };
+
+    // 代码块 A
+    {
+        let _b = Droppable { name: "b" };
+
+        // 代码块 B
+        {
+            let _c = Droppable { name: "c" };
+            let _d = Droppable { name: "d" };
+
+            println!("Exiting block B");
+        }
+        println!("Just exited block B");
+
+        println!("Exiting block A");
+    }
+    println!("Just exited block A");
+
+    // 手动使用 `drop` 函数来销毁。
+    drop(_a);
+
+    println!("end of the main function");
+
+    // `_a` *不会*在这里再次销毁
 }
