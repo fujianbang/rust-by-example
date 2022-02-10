1e49bde16de0cc4e6e4151ea481fd96098c54b09
付建邦 <fujianbang0ops@gmail.com>
2022-02-10 23:40:57 +0800

    finished module Traits - impl traits

--- src/traits/impl_trait/src/main.rs
+++ src/traits/impl_trait/src/main.rs
@@ -1,3 +1,46 @@
+use std::iter;
+use std::vec::IntoIter;
+
+fn combine_vecs_explict_return_type(
+    v: Vec<i32>,
+    u: Vec<i32>,
+) -> iter::Cycle<iter::Chain<IntoIter<i32>, IntoIter<i32>>> {
+    v.into_iter().chain(u.into_iter()).cycle()
+}
+
+// 通过返回类型 `impl Trait` 简化函数
+fn combine_vecs(
+    v: Vec<i32>,
+    u: Vec<i32>,
+) -> impl Iterator<Item=i32> {
+    v.into_iter().chain(u.into_iter()).cycle()
+}
+
+// 返回一个将输入和 `y` 相加的函数
+fn make_adder_function(y: i32) -> impl Fn(i32) -> i32 {
+    let closure = move |x: i32| { x + y };
+    closure
+}
+
+// 使用 impl Trait 返回使用 map 或 filter 闭包的迭代器
+fn double_positives<'a>(numbers: &'a Vec<i32>) -> impl Iterator<Item=i32> + 'a {
+    numbers
+        .iter()
+        .filter(|x| x > &&0)
+        .map(|x| x * 2)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let v1 = vec![1, 2, 3];
+    let v2 = vec![4, 5];
+    let mut v3 = combine_vecs(v1, v2);
+    assert_eq!(Some(1), v3.next());
+    assert_eq!(Some(2), v3.next());
+    assert_eq!(Some(3), v3.next());
+    assert_eq!(Some(4), v3.next());
+    assert_eq!(Some(5), v3.next());
+    println!("all done");
+
+    let plus_one = make_adder_function(1);
+    assert_eq!(plus_one(2), 3);
 }
