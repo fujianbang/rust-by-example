5f797b4c652235f66d3279686b1d0b1ce6b6679c
付建邦 <fujianbang0ops@gmail.com>
2022-02-12 22:22:30 +0800

    finished module error - option unwrap - unpacking options

--- src/error_handling/option_unwrap/unpacking_options/src/main.rs
+++ src/error_handling/option_unwrap/unpacking_options/src/main.rs
@@ -1,3 +1,38 @@
+fn next_birthday(current_age: Option<u8>) -> Option<String> {
+    let next_age: u8 = current_age?;
+    Some(format!("Next year I will be {}", next_age))
+}
+
+struct Person {
+    job: Option<Job>,
+}
+
+#[derive(Clone, Copy)]
+struct Job {
+    phone_number: Option<PhoneNumber>,
+}
+
+#[derive(Clone, Copy)]
+struct PhoneNumber {
+    area_code: Option<u8>,
+    number: u32,
+}
+
+impl Person {
+    fn work_phone_area_code(&self) -> Option<u8> {
+        self.job?.phone_number?.area_code
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let p = Person {
+        job: Some(Job {
+            phone_number: Some(PhoneNumber {
+                area_code: Some(61),
+                number: 439222222,
+            }),
+        }),
+    };
+
+    assert_eq!(p.work_phone_area_code(), Some(61));
 }
