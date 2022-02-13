503e8e7fc172408e74d75d1982627f340429baa8
付建邦 <fujianbang0ops@gmail.com>
2022-02-13 17:59:18 +0800

    finished module error - option unwrap - combinators maps

--- src/error_handling/option_unwrap/combinators_map/src/main.rs
+++ src/error_handling/option_unwrap/combinators_map/src/main.rs
@@ -1,3 +1,67 @@
+#![allow(dead_code)]
+
+#[derive(Debug)]
+enum Food { Apple, Carrot, Potato }
+
+// 去皮
+#[derive(Debug)]
+struct Peeled(Food);
+
+// 切碎
+#[derive(Debug)]
+struct Chopped(Food);
+
+// 烹饪
+#[derive(Debug)]
+struct Cooked(Food);
+
+// 削皮，如果没有食物，就返回 `None`，否则返回削好皮的食物
+fn peel(food: Option<Food>) -> Option<Peeled> {
+    match food {
+        Some(food) => Some(Peeled(food)),
+        None => None,
+    }
+}
+
+// 切碎
+fn chop(peeled: Option<Peeled>) -> Option<Chopped> {
+    match peeled {
+        Some(Peeled(food)) => Some(Chopped(food)),
+        None => None,
+    }
+}
+
+// 烹饪
+fn cook(chopped: Option<Chopped>) -> Option<Cooked> {
+    chopped.map(|Chopped(food)| Cooked(food))
+}
+
+// 处理流程
+fn process(food: Option<Food>) -> Option<Cooked> {
+    food.map(|f| Peeled(f))
+        .map(|Peeled(f)| Chopped(f))
+        .map(|Chopped(f)| Cooked(f))
+}
+
+// 吃
+fn eat(food: Option<Cooked>) {
+    match food {
+        Some(food) => println!("Mmm. I love {:?}", food),
+        None => println!("Oh not! It wasn't edible."),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let apple = Some(Food::Apple);
+    let carrot = Some(Food::Carrot);
+    let potato = None;
+
+    let cooked_apple = cook(chop(peel(apple)));
+    let cooked_carrot = cook(chop(peel(carrot)));
+
+    let cooked_potato = process(potato);
+
+    eat(cooked_apple);
+    eat(cooked_carrot);
+    eat(cooked_potato);
 }
