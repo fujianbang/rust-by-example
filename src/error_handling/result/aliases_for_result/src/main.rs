7ff1eb9b35d3099459afe8cbf59fae8270bb10d5
付建邦 <fujianbang0ops@gmail.com>
2022-02-13 18:40:37 +0800

    finished module error - result - aliases for result

--- src/error_handling/result/aliases_for_result/src/main.rs
+++ src/error_handling/result/aliases_for_result/src/main.rs
@@ -1,3 +1,21 @@
+use std::num::ParseIntError;
+
+type AliasedResult<T> = Result<T, ParseIntError>;
+
+fn multiple(first_number_str: &str, second_number_str: &str) -> AliasedResult<i32> {
+    first_number_str.parse::<i32>().and_then(|first_number| {
+        second_number_str.parse::<i32>().map(|second_number| first_number * second_number)
+    })
+}
+
+fn print(result: AliasedResult<i32>) {
+    match result {
+        Ok(n) => println!("n is {}", n),
+        Err(e) => println!("Error: {}", e),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    print(multiple("10", "2"));
+    print(multiple("t", "2"));
 }
