5ebb61767ce95939cebc660642c0241ed1ad5134
付建邦 <fujianbang0ops@gmail.com>
2022-02-13 19:59:31 +0800

    finished module error - iterating over results

--- src/error_handling/iterating_over_results/src/main.rs
+++ src/error_handling/iterating_over_results/src/main.rs
@@ -1,3 +1,57 @@
+fn iter1() {
+    let strings = vec!["tofu", "93", "18"];
+    let numbers: Vec<_> = strings
+        .into_iter()
+        .map(|s| s.parse::<i32>())
+        .collect();
+    println!("Result: {:?}", numbers);
+}
+
+fn iter2() {
+    let strings = vec!["tofu", "93", "18"];
+    let numbers: Vec<_> = strings
+        .into_iter()
+        // 忽略失败的项
+        .filter_map(|s| s.parse::<i32>().ok())
+        .collect();
+    println!("Results: {:?}", numbers);
+}
+
+fn iter3() {
+    let strings = vec!["tofu", "93", "18"];
+    let numbers: Result<Vec<_>, _> = strings
+        .into_iter()
+        .map(|s| s.parse::<i32>())
+        .collect();
+    println!("Results: {:?}", numbers);
+}
+
+fn iter4() {
+    let strings = vec!["tofu", "93", "18"];
+    let (numbers, errors): (Vec<_>, Vec<_>) = strings
+        .into_iter()
+        .map(|s| s.parse::<i32>())
+        .partition(Result::is_ok);
+    println!("Numbers: {:?}", numbers);
+    println!("ERRORS: {:?}", errors);
+}
+
+fn iter5() {
+    let strings = vec!["tofu", "93", "18"];
+    let (numbers, errors): (Vec<_>, Vec<_>) = strings
+        .into_iter()
+        .map(|s| s.parse::<i32>())
+        .partition(Result::is_ok);
+    let numbers: Vec<_> = numbers.into_iter().map(Result::unwrap).collect();
+    let errors: Vec<_> = errors.into_iter().map(Result::unwrap_err).collect();
+    println!("Numbers: {:?}", numbers);
+    println!("Errors: {:?}", errors);
+}
+
 fn main() {
-    println!("Hello, world!");
+    iter1();
+    iter2();
+    iter3();
+    iter4();
+    iter5();
 }
