94e55e0fce72d9fce7c52c48d66437390ce786ea
付建邦 <fujianbang0ops@gmail.com>
2022-02-14 22:06:44 +0800

    finished module std - strings

--- src/std/strings/src/main.rs
+++ src/std/strings/src/main.rs
@@ -1,3 +1,32 @@
 fn main() {
-    println!("Hello, world!");
+    let pangram: &'static str = "the quick brown fox jumps over the lazy dog";
+    println!("Pangram: {}", pangram);
+
+    println!("Words in reverse");
+    for word in pangram.split_whitespace().rev() {
+        println!("> {}", word);
+    }
+
+    // 复制字符到vector，排序并移除重复值
+    let mut chars: Vec<char> = pangram.chars().collect();
+    chars.sort();
+    chars.dedup();
+
+    // 创建一个空的String
+    let mut string = String::new();
+    for c in chars {
+        string.push(c);
+        string.push_str(",");
+    }
+
+    let chars_to_trim: &[char] = &[' ', ','];
+    let trimmed_str: &str = string.trim_matches(chars_to_trim);
+    println!("Used characters: {}", trimmed_str);
+
+    // 堆分配一个字符串
+    let alice = String::from("I like dogs");
+    let bob: String = alice.replace("dog", "cat");
+
+    println!("Alice says: {}", alice);
+    println!("Bob says: {}", bob);
 }
