bbde3b0ae6463f7477681a9db52dcc327f504483
付建邦 <fujianbang0ops@gmail.com>
2022-02-17 22:09:44 +0800

    finished module std misc - threads

--- src/std_misc/threads/src/main.rs
+++ src/std_misc/threads/src/main.rs
@@ -1,3 +1,17 @@
+use std::thread;
+
+static NTHREAD: i32 = 10;
+
 fn main() {
-    println!("Hello, world!");
+    let mut children = vec![];
+
+    for i in 0..NTHREAD {
+        children.push(thread::spawn(move || {
+            println!("this is thread number {}", i)
+        }));
+    }
+
+    for child in children {
+        let _ = child.join();
+    }
 }
